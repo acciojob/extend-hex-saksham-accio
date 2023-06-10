bbc3cd6516a8ece422943d094b2bcd7d8b81d0dd
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2023-06-10 09:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+const extendHex = (shortHex) =>
+  "#" +
+  shortHex
+    .slice(shortHex.startsWith("#") ? 1 : 0)
+    .split("")
+    .map((x) => x + x)
+    .join("");
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
